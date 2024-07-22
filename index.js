47d9efd09a52217b877ecdaa1e0250f7d1849afe
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-07-23 04:24:56 +0500

    end

---
+++ index.js
@@ -0,0 +1,134 @@
+// For ga oid masalalar
+
+// 1 masala
+
+// let sum = 0;
+// for(let i = 1; i <= 100; i++){
+//     if(i % 2 == 1){
+//         sum = sum + i;
+//     }
+//     console.log(sum);
+// }
+
+// 2 masala
+
+// let num = +prompt("son kiriting");
+// let res = 1;
+// for (let i = 1; i <= num; i++){
+//     res = res * i;
+// }
+// console.log(res);
+
+// 4 masala
+
+// let sum = 0;
+// let p = 1;
+// for(let i = -2; i <= 20; i++){
+//     p = i**2;
+//     sum = sum + p;
+// }
+// console.log("kvadrati", p);
+// console.log("yeg'indisi", sum);
+
+
+
+
+// 7 masala
+
+// let num = prompt("sonni kiriting");
+// let t = "";
+// for (let i = num.length - 1; i >= 0; i--) {
+//     t += num[i];
+// }
+// console.log(t);
+
+// 9 masala
+
+// for(let i = -1; i <= 50; i++){
+//     if (i % 3 == 0 && i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+// 10 masala
+
+// let counter = 0;
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+//   counter++;
+// }
+// let result = sum / counter;
+// console.log(result);
+
+// 11 masala
+
+// let num = prompt('sonni kiriting');
+// let length = num.length;
+// console.log(length);
+
+// 12 masala
+
+// let num = +prompt("sonni kiriting");
+// let m = 0;
+// m = m - num;
+// console.log(m);
+
+// 14 masala
+
+// for (let i = -1; i <=200; i++){
+//     if(i % 2 == 0 )
+//         console.log(i);
+// }
+
+// Break/continue ga oid masalalar.
+
+// 1 masala
+
+// for(let i = -1; i <= 10; i++){
+//     if(i == 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// 2 masala
+
+// for(let i = -1; i <= 10; i++){
+//     if(i == 3 || i == 7){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 3 masala
+
+// let res = "";
+// for(let i = 1; i <= 1; i++){
+//     let a = +prompt("a");
+//     if(a == 7){
+//         break;
+//         res = "topildi";
+//     } else {
+//         res = "topilmadi";
+//     }
+//     console.log(res);
+// }
+
+// 4 masala
+
+// for(let i = -1; i <= 10; i++){
+//     if(i % 2 == 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 5 masala
+
+// for(let i = -1; i <= 100; i++){
+//     if(i > 50){
+//         break;
+//     }
+//     console.log(i);
+// }
